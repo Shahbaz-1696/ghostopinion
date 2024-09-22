0a610f2dba705670875be84c4ff68df339ae3953
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-22 23:35:54 +0530

    Completed home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,55 @@
+"use client";
 import React from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import messages from "@/lib/messages.json";
+import AutoPlay from "embla-carousel-autoplay";
+
 export default function Home() {
-  return <h1>Hi there</h1>;
+  return (
+    <>
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="font-bold text-3xl md:text-5xl">
+            Dive into the World of Anonymous Conversations
+          </h1>
+          <p className="mt-3 md:mt-4 text-base md:text-lg">
+            Explore GhostOpinion - Where your identity remains a secret.
+          </p>
+        </section>
+        <Carousel
+          plugins={[AutoPlay({ delay: 2000 })]}
+          className="w-full max-w-xs"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardHeader>{message.title}</CardHeader>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-lg font-semibold">
+                        {message.content}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </main>
+      <footer className="text-center p-4 md:p-6">
+        @2024 GhostOpinion. All rights reserved
+      </footer>
+    </>
+  );
 }
